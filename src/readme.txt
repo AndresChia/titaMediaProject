2785479ee731f858f85a192069564c0403ac348e
Andres_Chia <andres_chia11@hotmail.com>
2020-02-24 11:24:56 -0500

    COMMIT: readme

---
+++ src/readme.txt
@@ -0,0 +1,5 @@
+
+Pasos a seguir para levantar la base de datos y el servidro de reactJs
+
+1. yarn start
+2. json-server --watch db.json --port 3001
--- src/services/apiDb.js
+++ src/services/apiDb.js
@@ -1,3 +1,4 @@
-const url = 'http://localhost:3000';
+const url = 'http://localhost:3001';
+
 
 export const getData = () => fetch(`${url}/images`).then(v => v.json());
